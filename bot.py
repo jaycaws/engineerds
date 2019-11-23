8de0a957fa91db0f4506cc59c94eb2951a5f00c2
Ryan <ryanthomasbrown.career@gmail.com>
2019-11-22 17:28:42 -0800

    Added additional logic for scoping channel visibility and game logic

---
+++ bot.py
@@ -0,0 +1,34 @@
+import discord
+import 
+
+class MyClient(discord.Client):
+    def __init__(self):
+        self.activeChannels = [
+            {
+                id:"engineerds",
+                player:"*",
+                state:None
+            }
+        ]
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print(message.channel.id)
+        for channel in self.activeChannels:
+            if message.channel.id == channel.id:
+                if channel.state == None:
+                    self.process_command(message)
+                else:
+                    self.startNims(message)
+
+    
+
+    async def process_command(message):
+        print('Message from {0.author}: {0.content}'.format(message))
+
+    async def startNims(message):
+        print('We starting nims bitches')
+
+client = MyClient()
+client.run('NjQ3NTkyMjI5MTI4NjM0MzY5.Xdh7WQ.WV3JqYPljdoKBWIr6MvvAyG4ufs')
